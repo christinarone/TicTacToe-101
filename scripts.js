1061056c8a430ad127bddab429f07ee6afe97308
christinarone <96158586+christinarone@users.noreply.github.com>
2022-01-25 21:12:12 -0600

    final

--- scripts.js
+++ scripts.js
@@ -50,8 +50,11 @@ const addMarker = (id) => {
   // .getElementById(id)
   // document
   // .innerHTML 
+  document.getElementById(id).innerHTML = currentMarker;
+  // document.getElementById("greeting");
 
   changeMarker()
+
 }
 
 
@@ -83,7 +86,7 @@ const changeMarker = () => {
 
 // This "resetBoard" function is called when the user clicks on the "Restart" button.
 const resetBoard = () => {
-  
+  const squares = document.getElementsByTagName("TD");
   // @TODO-3: To make your "Restart" button work you'll need to build a line of code here that:
       // collects all of the "td" elements into an HTML Collection: https://www.w3schools.com/jsref/dom_obj_htmlcollection.asp  
     
